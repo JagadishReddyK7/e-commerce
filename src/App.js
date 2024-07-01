5971da46fae4fb29d4c99b4974643221ec89f7de
JagadishReddyK7 <kollu.jagadishreddy@gmail.com>
2024-07-01 21:11:34 +0530

    started e-commerce app and created Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Shop/>}/>
+          <Route path='/women' element={<ShopCategory/>}/>
+          <Route path='/kids' element={<ShopCategory/>}/>
+          <Route path='/men' element={<ShopCategory/>}/>
+        </Routes>
+      
+      </BrowserRouter>
     </div>
   );
 }
